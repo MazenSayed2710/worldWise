15458c765457c28052831b77fa3f22e8953d5104
Mazen Mohamed <ma3en.mohamed@gmail.com>
2026-05-20 18:27:08 +0300

    update

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,5 +1,10 @@
 import TripDetails from "@/components/TripDetails";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <TripDetails />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <TripDetails />;
+    </Suspense>
+  );
 }
